8cefe6436d63cf07ac382bc32416c98591a40bc6
ashikh <ashee.vajra@gmail.com>
2016-06-28 10:17:03 +0530

    Update app.js

--- Angular-input submit/app.js
+++ Angular-input submit/app.js
@@ -1,6 +1,10 @@
 var app=angular.module('myapp', []);
 	app.controller('mycontroller', function($scope)
 		{
-	$scope.name='Ravi';
-	$scope.place='Bangalore';
+	$scope.submitAll=function(){
+		var name1=document.getElementById('name').value;
+		var place1=document.getElementById('place').value;
+		document.getElementById('names').innerHTML=name1;
+		document.getElementById('places').innerHTML=place1;
+	}
 	});
